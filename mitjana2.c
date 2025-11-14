9bad682d0ca0f9e6313db132e53b9a6f39efb36c
shave7 <javbolren@alu.edu.gva.es>
2025-11-14 12:10:42 +0100

    Create mitjana2.c

---
+++ mitjana2.c
@@ -0,0 +1,14 @@
+   /********************************
+     * Programa q calcula la mitjana
+     * artmètica de 2 enters
+     * Xavi Blanes PXSI 25-26 ******/
+   #include <stdio.h>
+   void main(){
+        int a, b, mitjana;
+        printf("1r núm?:");
+        scanf("%d", &a);
+        printf("2n núm?: ");
+        scanf("%d", &b);
+        printf("La mitjana és: %d/n", (a+b)/2);
+        return;
+}
